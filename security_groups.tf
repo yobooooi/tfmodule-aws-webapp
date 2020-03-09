54a517043c1a6d28f4467cf8d6640817b0513d71
Adan <adan@synthesis.co.za>
2020-03-09 14:24:42 +0200

    NFS mount SG added and Internet Access

--- security_groups.tf
+++ security_groups.tf
@@ -21,7 +21,24 @@ resource "aws_security_group" "app_server" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 2049
+        to_port = 2049
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port = 3306
         to_port = 3306
