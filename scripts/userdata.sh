a816b5d38b8d54770b39897f4590378a8ee201f3
Adan <adan@synthesis.co.za>
2020-04-14 15:18:27 +0200

    Updated directory where site is being served from

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+sudo mkdir -p /data/www/html/${site_name}
 
-sudo mkdir -p /data/www/html/rwsanlam
-
-aws s3api get-object --bucket wordpress-deployment-bucket --key rwsanlam.zip /data/www/html/rwsanlam/rwsanlam.zip
-unzip /data/www/html/rwsanlam/rwsanlam.zip -d /data/www/html/rwsanlam/ 
+aws s3api get-object --bucket wordpress-deployment-bucket --key ${site_name}.zip /data/www/html/${site_name}/${site_name}.zip
+unzip /data/www/html/${site_name}/${site_name}.zip -d /data/www/html/${site_name}/ 
 
 aws s3api get-object --bucket wordpress-deployment-bucket --key config/httpd.conf /etc/httpd/conf/httpd.conf
 systemctl restart httpd
