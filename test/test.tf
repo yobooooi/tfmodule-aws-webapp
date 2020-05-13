5450bb7d3fe509d3d0f0ab6d0e223954efa26793
Adan Patience <adan@synthesis.co.za>
2020-05-13 16:06:25 +0000

    Remote state added in test

--- test/test.tf
+++ test/test.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "sb001-tf-state-bucket"
+    key    = "wordpress-poc"
+    region = "eu-west-1"
+    profile = "sanlam-sb001"
+  }
+}
+
 provider "aws" {
     region  = "eu-west-1"
     profile = "sanlam-sb001"
