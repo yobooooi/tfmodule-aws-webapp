9cf6b4d3775f952c450e430ce1bf56bc194d5bc0
Adan Patience <apatience3.ap@gmail.com>
2021-05-23 19:57:23 +0200

    updated userdata

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo mount -t efs ${efs_id}:/ /var/www/html/
+#sudo wget -O /tmp/latest.tar.gz https://wordpress.org/latest.tar.gz
+#sudo tar -xzvf /tmp/latest.tar.gz -C /var/www/html --strip-components
